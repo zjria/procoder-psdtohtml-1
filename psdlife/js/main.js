e297bd23f20555bf540c64338120595a3b2418f7
zjria <75481630+zjria@users.noreply.github.com>
2020-12-04 18:38:10 +0600

    Add files via upload

---
+++ psdlife/js/main.js
@@ -0,0 +1,11 @@
+// Bx slider
+
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+    	meanMenuContainer: '#mobile_manu',
+    	meanScreenWidth: 991,
+
+
+    });
+  }); 
